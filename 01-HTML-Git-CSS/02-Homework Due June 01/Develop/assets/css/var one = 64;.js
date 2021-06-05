4ccf6c7272744eee0a77b0761df4e331a89368a4
Cialysse <marshall.cierra714@gmail.com>
2021-06-05 09:48:13 -0400

    Javascript 3

---
+++ 01-HTML-Git-CSS/02-Homework Due June 01/Develop/assets/css/var one = 64;.js
@@ -0,0 +1,30 @@
+var one = 64;
+var two = "656302";
+var three = false;
+var four = 64.55;
+var five = "Howdy!";
+var six;
+
+// Insert comments to explain what each console log below will log to the console
+console.log(typeof one); 
+// Logs number 
+console.log(typeof two); 
+//logs string
+console.log(typeof three); 
+//logs boolean
+console.log(typeof four); 
+//logs number
+console.log(typeof five);
+//logs string
+console.log(typeof six);
+//logs undefined
+
+//reassign variables
+four = "Hello!";
+five = false;
+six = 23;
+
+// Insert comments to explain what each console log below will log to the console
+console.log(typeof four);
+console.log(typeof five);
+console.log(typeof six);
